0ab8932ad9967b7f34cc896afdecc9f2b2daf458
HyunSul <me_2_day_@naver.com>
2021-04-19 11:12:33 +0900

    Day-4

--- flask_web/app.py
+++ flask_web/app.py
@@ -4,6 +4,9 @@ app = Flask(__name__) #__&&__ 는 내장변수
 
 app.debug = True #오류를 웹폐이지 상에 띄워줌(단점 나의 폴더 목록도 다 보이기 떄문에 개발 할 때만 True)
 
+@app.route('/data', methods=['GET']) #route : 중계하다 , @ : decorate // /data 폴더의 뒤로 경로 지정
+def index():
+    return "Welcom to hell"
 
 if __name__ == '__main__': #모듈의 시작점을 만듦, 프로그램의 시작점일 떄만 아래의 코드 실행
     app.run()
